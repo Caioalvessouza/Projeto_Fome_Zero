95ea7dd01ceab19e53e90e1c97b6b226b920157a
Caio Rodrigues Alves de Souza <138089037+Caioalvessouza@users.noreply.github.com>
2023-12-10 16:58:56 -0300

    Update 01_📊_Home.py

--- 01_📊_Home.py
+++ 01_📊_Home.py
@@ -11,10 +11,11 @@ import plotly.express as px
 #from utils.process_data import process_data
 from collections import Counter
 
-csv_path ='C:/Users/Caio/Documents/cientista de dados/phyton/Projeto do aluno/zomato/data.csv'
+csv_path = 'C:/Users/Caio/Documents/cientista de dados/phyton/Projeto do aluno/zomato/data.csv'
+
 
 # Leia o arquivo CSV e crie o DataFrame
-#df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path)
 
 
 
